26f117d11daf07430213365d7d53b7a8189802dd
hitoiki <hitoiki103@gmail.com>
2019-11-30 20:13:05 +0900

    ProjectStart

---
+++ Assets/Script/MoveDemo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDemo : MonoBehaviour
+{
+    //テスト用なので後で改善する
+    public Rigidbody rb;
+    public float speed;
+
+    private Vector3 direction;
+    // Start is called before the first frame update
+    void Update(){
+        KeyVectorCheck();
+    }
+
+    // Update is called once per frame
+    Vector3 KeyVectorCheck()
+    {
+        direction = Vector3.zero;
+         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
+        {
+            direction += Vector3.up;
+        }
+
+        if (Input.GetKey(KeyCode.S)){
+            direction += Vector3.up * (-1.0f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector3.right * (-1.0f);
+        }
+        return direction;
+    }
+
+    void FixedUpdate(){
+        if(KeyVectorCheck() != Vector3.zero){
+        rb.velocity = KeyVectorCheck()*speed;
+        }
+    }
+}
---
+++ Assets/Script/TouchSenser.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TouchSenser : MonoBehaviour
+{
+    //接触の状況を確認するメソッド
+    public bool stumpready;
+
+    //　レイを伸ばして接地判定に用いる
+    private Transform rayPosition;
+
+    public float rayRange = 1.0f; 
+    public Transform demoHit;
+    private float distanceFromTargetObj;
+    public float raysize;
+    private RaycastHit hit;
+    void Update()
+    {
+        demoHit = ToucherBox(rayPosition,Vector3.down * rayRange,raysize);
+        if(demoHit != null){
+        Debug.Log(demoHit.name);
+        }
+     }
+
+    Transform ToucherBox(Transform target,Vector3 Vec,float size){
+        
+        //boxcastにして、isTriggerは無視するように改良した     
+        if(Physics.BoxCast (target.position - ((Vec.normalized) * size), Vector3.one * size * (0.5f) , (Vec.normalized) ,out hit , Quaternion.identity , Vec.magnitude))
+        {
+            /*if()//空中から接地したら、踏む準備する
+            {
+                stumpready = true;
+            }*/
+        distanceFromTargetObj = hit.distance;
+        return hit.transform;
+
+        }else{
+        distanceFromTargetObj = Vec.magnitude;
+        return null;
+        }
+    }
+
+    void OnDrawGizmos() 
+    {
+        //レイを疑似的に視覚化
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position +((Vector3.down) * distanceFromTargetObj) - Vector3.down*(raysize), Vector3.one * raysize);
+    }
+
+}
